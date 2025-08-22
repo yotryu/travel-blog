060e6e51737105398da318cc700c92cdcbe94af4
Jonathan Law <yotryu@gmail.com>
2025-08-22 22:16:40 +1000

    Added support for deploying using gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,28 @@
+import fs from 'fs';
+import path from 'path';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+let allCollections = JSON.parse(fs.readFileSync(import.meta.dirname + "/static/collections/all.json"));
+let allPosts = [];
+
+allCollections.forEach(i => {
+	i.posts.forEach(p => {
+		allPosts.push(`/blog/post/${p}`);
+	});
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			entries: ["/", ...allPosts]
+		}
+	}
 };
 
 export default config;
